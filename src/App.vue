818560c92c1b286f10443277905eb5cb62f512a3
정지우 <cheltus11@gmailcom>
2024-07-10 18:29:27 +0900

    init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <p>{{  message  }}</p>
+    <button @click="changeMessage">change</button>
+
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    setup(){
+      const message = ref('hello');
+
+    }
+    
+    // data() {
+    //   return {
+    //     message: 'hello'
+    //   }
+    // },
+    // methods: {
+    //   changeMessage() {
+    //     this.message = 'hi'
+    //   }
+    // }
   }
-}
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+
 </style>
